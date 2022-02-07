ea5c096cb4617ff61400fa480c50ef72558a3abe
Ahsan-J <ahsan.ahmed99@ymail.com>
2022-02-07 20:22:45 +0500

    added pagination for books

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
 import { CreateBookBody } from "./book.dto";
 import { BookService } from "./book.service";
 import { ApiBearerAuth, ApiSecurity, ApiTags } from "@nestjs/swagger";
@@ -6,6 +6,7 @@ import { AuthGuard, UseAccess, UseRoles } from "../auth/auth.guard";
 import { UserRole } from "../user/user.enum";
 import { Book } from "./book.entity";
 import { APIAccessLevel } from "../apikey/api.enum";
+import { PaginationMeta, PaginationQuery } from "src/helper-modules/common/common.dto";
 
 @ApiTags('Book')
 @ApiBearerAuth('AccessToken')
@@ -21,5 +22,22 @@ export class BookController {
     createBook(@Body() createBook: CreateBookBody): Promise<Book> {
         return this.bookService.createBook(createBook);
     }
-    
+
+    @Get('all')
+    @UseRoles(UserRole.User, UserRole.Admin, UserRole.SuperAdmin)
+    async getAllBooks(@Query() query: PaginationQuery): Promise<Array<Book> | { meta: PaginationMeta }> {
+        const page = parseInt(query.page);
+        const pageSize = parseInt(query.pageSize || '10');
+
+        const [data, meta] = await this.bookService.getBooks({
+            skip: (page - 1) * pageSize,
+            take: page * pageSize
+        });
+
+        return {
+            ...data,
+            meta
+        }
+    }
+
 }
--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,7 +1,9 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Inject, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { nanoid } from "nanoid";
-import { Repository } from "typeorm";
+import { PaginationMeta } from "src/helper-modules/common/common.dto";
+import { CommonService } from "src/helper-modules/common/common.service";
+import { FindManyOptions, Repository } from "typeorm";
 import { AuthorService } from "../author/author.service";
 import { GenreService } from "../genre/genre.service";
 import { CreateBookBody } from "./book.dto";
@@ -14,14 +16,16 @@ export class BookService {
         private bookRepository: Repository<Book>,
         private authorService: AuthorService,
         private genreService: GenreService,
-    ) {}
+        @Inject(CommonService)
+        private commonService: CommonService
+    ) { }
 
-    async createBook(createBook: CreateBookBody) : Promise<Book> {
+    async createBook(createBook: CreateBookBody): Promise<Book> {
 
         const author = await this.authorService.getAuthor(createBook.author);
 
-        const isbnBook = await this.bookRepository.findOne({where:{ isbn: createBook.isbn }})
-        
+        const isbnBook = await this.bookRepository.findOne({ where: { isbn: createBook.isbn } })
+
         if (isbnBook) {
             throw new BadRequestException("ISBN  already exist");
         }
@@ -44,10 +48,18 @@ export class BookService {
 
     async getBookById(id: Book['id']): Promise<Book> {
         if (!id) throw new BadRequestException('"Book id" is missing');
-        const book = await this.bookRepository.findOne(id, {relations:['genre']});
+        const book = await this.bookRepository.findOne(id, { relations: ['genre'] });
 
         if (!book) throw new BadRequestException("No Book find for specific id");
         return book;
+    }
+
+    async getBooks(options: FindManyOptions<Book>): Promise<[Book[],PaginationMeta]> {
+        const [result, count] = await this.bookRepository.findAndCount(options);
+
+        const meta = this.commonService.generateMeta(count, options.skip, options.take);
+
+        return [result, meta]
     }
 
     // async addGenre(genre: UserGenre['name']): Promise<Book> {
